724e4b82b63eb3e00799ff636793720cdecd73a9
Chandra Shekhar Murmu <chandra0murmu@gmail.com>
2022-06-28 21:52:34 +0200

    Added Read_Halos_Buffered():
    Not tested.

--- main.cc
+++ main.cc
@@ -1,23 +1,5 @@
-#include <valarray>
-#include <iostream>
-#include <fstream>
-#include <cstdint>
-
-uint64_t N_halo ;
-
-void Read_Halos( const std::string,
-								 std::valarray<float>&,
-								 std::valarray<float>&,
-								 std::valarray<float>&,
-								 std::valarray<float>&  ) ;
 
 int main( int argc, char *argv[] ) {
 
-	std::valarray<float> Pos_x, Pos_y, Pos_z,
-		Lum ;
-
-	std::string filename = argv[1] ;
-
-	Read_Halos( filename,
-							Pos_x, Pos_y, Pos_z, Lum ) ;
+	
 }
